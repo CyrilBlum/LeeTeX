d3159c9f41692f8b9a2d82aa987888309f6ae417
ThomasGrafNumerics <thomas.graf.numerics@gmail.com>
2024-08-30 14:30:37 +0200

    improving image helper

--- GrundlagenInfo/00_Programmieren/BilderBearbeiten/eigene_filter.py
+++ GrundlagenInfo/00_Programmieren/BilderBearbeiten/eigene_filter.py
@@ -1,37 +1,34 @@
 import image_helper as ih
 
 
-# def count_black_pixels(image):
-#     # count and return the number of black pixels
-#     # of a given black / white image
-#
-#     pixel_list = ih.load_bw_pixels(image)
-#     black_pixels = 0
-#
-#     for pixel in pixel_list:
-#         if pixel == 0:
-#             black_pixels += 1
-#
-#     return black_pixels
-#
-#
-# def invert_black_white_image(image):
-#     inverted = []
-#     pixel_list = ih.load_bw_pixels(image)
-#
-#     for pixel in pixel_list:
-#         if pixel == 0:
-#             inverted.append(1)
-#         else:
-#             inverted.append(0)
-#
-#     w, h = image.size
-#     inverted_image = ih.new_bw_image(w, h, inverted)
-#     ih.show_images_vertically(image, inverted_image)
+def count_black_pixels(filename):
+    # count and return the number of black pixels
+    # of a given black / white image
+
+    pixel_list = ih.load_bw_pixels(filename)
+    black_pixels = 0
+
+    for pixel in pixel_list:
+        if pixel == 0:
+            black_pixels += 1
+
+    return black_pixels
 
 
-# print(count_black_pixels("Ivy_Black_White.jpg"))
+def invert_black_white_image(filename):
+    original = ih.load_bw_image(filename)
+    pixel_list = ih.load_bw_pixels(filename)
 
-# im = ih.Image.open("Ivy_Color.jpeg")
-# im = im.convert("1")
-# print(im.getdata()[0])
+    inverted = []
+    for pixel in pixel_list:
+        if pixel == 0:
+            inverted.append(1)
+        else:
+            inverted.append(0)
+
+    w, h = original.size
+    inverted_image = ih.new_bw_image(w, h, inverted)
+    ih.compare_images(original, inverted_image)
+
+
+invert_black_white_image("Effretikon_Black_White.jpg")
--- GrundlagenInfo/00_Programmieren/BilderBearbeiten/image_helper.py
+++ GrundlagenInfo/00_Programmieren/BilderBearbeiten/image_helper.py
@@ -1,97 +1,114 @@
 from PIL import Image
 import random
 
+# Monochrome Bilder
 
-## monochrome images (black / white only) ##
+
+def load_bw_pixels(filename):
+    im = Image.open(filename)
+    im = im.convert("1")
+    l = list(im.getdata())
+    return [1 if 255 == i else 0 for i in l]
 
 
-def load_monochrome_image(image_name):
-    im = Image.open(image_name)
+def load_bw_pixels_2d(filename):
+    im = load_bw_image(filename)
+    w, h = im.size
+    data = im.getdata()
+    return [[data[y * w + x] for y in range(h)] for x in range(w)]
+
+
+def load_bw_image(filename):
+    im = Image.open(filename)
     return im.convert("1")
 
 
-def load_monochrome_pixels(image_name):
-    im = load_monochrome_image(image_name)
-    L = list(im.getdata())
-    return [1 if 255 == i else 0 for i in L]
+def new_bw_pixels_2d(w, h):
+    return [[0 for y in range(h)] for x in range(w)]
 
 
-## grayscale images ##
-def load_grayscale_image(image_name):
-    im = Image.open(image_name)
+def new_bw_image(w, h, pixels=None):
+    im = Image.new("1", (w, h))
+    if pixels is not None:
+        if isinstance(pixels[0], list):
+            pixels = list(zip(*pixels))
+            pixels = [item for sublist in pixels for item in sublist]
+        im.putdata(pixels)
+    return im
+
+
+# Graustufen
+def load_grayscale_image(filename):
+    im = Image.open(filename)
     return im.convert("L")
 
 
-def load_grayscale_pixels(image_name):
-    im = load_grayscale_image(image_name)
+def load_grayscale_pixels(filename):
+    im = load_grayscale_image(filename)
     return list(im.getdata())
 
 
-## rgb images ##
-def load_rgb_image(image_name):
-    im = Image.open(image_name)
-    return im.convert("rgb")
-
-
-def load_rgb_pixels(image_name):
-    im = load_rgb_image(image_name)
-    return list(im.getdata())
-
-
-## universal utility ##
-# create new zero initialized 2d list of lists
-def new_pixels_2d(width, height):
-    return [[0] * height] * width
-
-
-# load image as 2d list of lists
-def load_pixels_2d(image_name):
-    im = load_monochrome_image(image_name)
-    width, height = im.size
+def load_grayscale_pixels_2d(filename):
+    im = load_grayscale_image(filename)
+    w, h = im.size
     data = im.getdata()
-    return [[data[y * width + x] for y in range(height)] for x in range(width)]
+    return [[data[y * w + x] for y in range(h)] for x in range(w)]
 
 
-# load image as 1d list of pixels
-def load_pixels_1d(image_name, mode):
-    if mode == "monochrome":
-        load_monochrome_pixels(image_name)
-    elif mode == "grayscale":
-        load_grayscale_pixels(image_name)
-    elif mode == "rgb":
-        load_rgb_pixels(image_name)
-    else:
-        print("Error: invalide mode (choose either: monochrome, grayscale or color)")
+def new_grayscale_pixels_2d(w, h):
+    return [[0 for y in range(h)] for x in range(w)]
 
 
-def new_image(width, height, mode):
-    if mode == "monochrome":
-        return Image.new("1", (width, height))
-    elif mode == "grayscale":
-        return Image.new("L", (width, height))
-    elif mode == "rgb":
-        return Image.new("rgb", (width, height))
-    else:
-        print("Error: invalide mode (choose either: monochrome, grayscale or color)")
+def new_grayscale_image(w, h, pixels=None):
+    im = Image.new("L", (w, h))
+    if pixels is not None:
+        if isinstance(pixels[0], list):
+            pixels = list(zip(*pixels))
+            pixels = [item for sublist in pixels for item in sublist]
+        im.putdata(pixels)
+    return im
 
 
+# RGB
+def load_rgb_image(filename):
+    im = Image.open(filename)
+    return im.convert("RGB")
+
+
+def load_rgb_pixels_2d(filename):
+    im = Image.open(filename)
+    w, h = im.size
+    data = im.getdata()
+    return [[data[y * w + x] for y in range(h)] for x in range(w)]
+
+
+def load_rgb_pixels(filename):
+    im = load_rgb_image(filename)
+    return list(im.getdata())
+
+
+def new_rgb_pixels_2d(w, h):
+    return [[(0, 0, 0) for y in range(h)] for x in range(w)]
+
+
+def new_rgb_image(w, h, pixels=None):
+    im = Image.new("RGB", (w, h))
+    if pixels is not None:
+        if isinstance(pixels[0], list):
+            pixels = list(zip(*pixels))
+            pixels = [item for sublist in pixels for item in sublist]
+        im.putdata(pixels)
+    return im
+
+
+# Misc:
 def random_pixel_value():
     return random.randint(0, 255)
 
 
-# show two images side by side to compare them
+# Display Helper:
 def compare_images(im1, im2):
-    dst = Image.new("rgb", (im1.width + im2.width, im1.height))
+    dst = Image.new("RGB", (im1.width + im2.width, im1.height))
     dst.paste(im1, (0, 0))
     dst.paste(im2, (im1.width, 0))
     dst.show()
-
-
-# def new_rgb_image(w, h, pixels=None):
-#     im = Image.new("rgb", (w, h))
-#     if pixels is not None:
-#         if isinstance(pixels[0], list):
-#             pixels = list(zip(*pixels))
-#             pixels = [item for sublist in pixels for item in sublist]
-#         im.putdata(pixels)
-#     return im
