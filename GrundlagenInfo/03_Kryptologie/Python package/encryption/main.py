fabdf075862853e35f9d9ab353f19c683f5d54c8
cyrilwendl <cyrilwendl@gmail.com>
2024-11-23 12:38:37 +0100

    Significant progress in crypto script and slides

--- GrundlagenInfo/03_Kryptologie/Python package/encryption/main.py
+++ GrundlagenInfo/03_Kryptologie/Python package/encryption/main.py
@@ -1,11 +1,24 @@
 from vigenere import *
 from caesar import *
+plt.style.use('ggplot')
+
+# kurzer Text (Skript Aufgabe)
+text_kurz = "SIEHABENESGESCHAFFTDIESENTEXTZUKNACKEN"
+text_kurz_c = caesar(text_kurz, 2)
+fig, ax = show_letter_freq(text_kurz_c)
+#plt.show()
+plt.savefig("../../Figures/caesar-freq-exercise.pdf", format="pdf", bbox_inches="tight")
+
+# langer Text, mit Caesar und Vigenère
 # langer Text einlesen aus .txt-Datei
 text_lang = open("text.txt").readlines()[0]
 text_lang = preprocess_text(text_lang)
 text_lang_c = caesar(text_lang, 2)
-plt.style.use('ggplot')
 fig, ax = show_letter_freq(text_lang_c)
 #plt.show()
 plt.savefig("../../Figures/caesar-freq.pdf", format="pdf", bbox_inches="tight")
 
+text_lang_v = vigenere(text_lang, "KEY")
+fig, ax = show_letter_freq(text_lang_v)
+#plt.show()
+plt.savefig("../../Figures/vigenere-freq.pdf", format="pdf", bbox_inches="tight")
