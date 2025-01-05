e1d2c08e9718ff45e92996c5889a6cd64191f538
cyrilwendl <cyrilwendl@gmail.com>
2025-01-05 20:04:42 +0100

    Major updates to crypto skript

---
+++ GrundlagenInfo/03_Kryptologie/Python package/encryption/umkehren.py
@@ -0,0 +1,11 @@
+def umkehren(klartext):
+    geheimtext = ""  # leerer String
+    b = len(klartext)-1
+    while b >= 0:
+        geheimtext += klartext[b]
+        b -= 1
+
+    print(geheimtext)
+
+# Verwendung: 
+umkehren("KANTONSSCHULE")
