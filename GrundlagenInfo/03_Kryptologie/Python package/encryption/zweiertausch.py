f28798cf9883c9e44511b8db7aff70bbae009698
cyrilwendl <cyrilwendl@gmail.com>
2025-01-09 22:43:36 +0100

    Various updates in crypto script and geography scripts

--- GrundlagenInfo/03_Kryptologie/Python package/encryption/zweiertausch.py
+++ GrundlagenInfo/03_Kryptologie/Python package/encryption/zweiertausch.py
@@ -1,14 +1,16 @@
- def zweiertausch(klartext):
-     geheimtext = "" # leerer String
-     b = 0
-     while b < (len(klartext) - 1):
-         geheimtext += klartext[b + 1] + klartext[b]
-         b += 2
- 
-     if len(klartext) % 2 != 0:
-         # Klartexte ungerader Länge
-         geheimtext += klartext[len(klartext) - 1]
-    print(geheimtext)
- 
-# Verwendung: 
+def zweiertausch(klartext):
+    geheimtext = ""  # leerer String
+    b = 0
+    while b < (len(klartext) - 1):
+        geheimtext += klartext[b + 1] + klartext[b]
+        b += 2
+
+    if len(klartext) % 2 != 0:
+        # Klartexte ungerader Länge
+        geheimtext += klartext[len(klartext) - 1]
+
+
+print(geheimtext)
+
+# Verwendung:
 # zweiertausch("KANTONSSCHULE")
