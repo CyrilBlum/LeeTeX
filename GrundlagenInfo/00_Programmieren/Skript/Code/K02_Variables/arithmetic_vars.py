29df4e1d9b31a95ec96229a5b5a9fe5120a217b1
cyrilwendl <cyrilwendl@gmail.com>
2025-02-20 16:46:52 +0100

    Various updates, myanswer moved to end of chapters

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K02_Variables/arithmetic_vars.py
@@ -0,0 +1,5 @@
+res1 = 17 - 3 * 8
+res2 = (5-2) * 4 + 1
+res3 = 3**6
+res4 = 18**(2+3)
+print(res1, res2, res3, res4)
---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K02_Variables/modulo_pizza.py
@@ -0,0 +1,3 @@
+p = 4 # Beispielwert, Anzahl Personen
+s = 33 # Beispielwert
+uebrige_stuecke = s % p # übriggebliebene Pizza-Stücke
