4946ea19d479c1761c9bd82054c94d6db1270fef
cyrilwendl <cyrilwendl@gmail.com>
2025-02-21 08:05:57 +0100

    Added Python exercises for chapte 3, need to be included in script

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K03_Logic/koffer.py
@@ -0,0 +1,17 @@
+def koffer(gewicht):
+    # überprüft, ob "gewicht" eine Zahl ist
+    if not isinstance(gewicht, (int, float)):
+        print("Das eingegebene Gewicht ist nicht zulässig!")
+    elif gewicht > 100:
+        print("Der Koffer ist zu schwer")
+    elif gewicht > 20:
+        print("Ihr Koffer hat", gewicht-20, "kg Übergewicht. Das kostet",5*(gewicht-20),"Franken.")
+    elif gewicht > 0:
+        print("Der Koffer ist gratis")
+    else:
+        print("Das eingegebene Gewicht ist nicht zulässig!")
+
+koffer(20)
+koffer(26)
+koffer(-3)
+koffer("HAHA")
