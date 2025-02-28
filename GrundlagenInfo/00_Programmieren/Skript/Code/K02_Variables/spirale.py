7076cafd5d1eda88024843b3f012db587e8cecba
cyrilwendl <cyrilwendl@gmail.com>
2025-02-28 11:43:29 +0100

    Various bugfixes, added exercise for += expression

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K02_Variables/spirale.py
@@ -0,0 +1,11 @@
+import turtle as t
+
+t.speed(100)
+
+laenge = 10
+for _ in range(20):
+    t.fd(laenge)
+    t.rt(90)
+    laenge+=10
+
+t.done()
---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K02_Variables/spirale_fill.py
@@ -0,0 +1,7 @@
+import turtle as t
+
+laenge = 1
+for _ in range(20):
+    # HIER vervollständigen
+
+t.done()
