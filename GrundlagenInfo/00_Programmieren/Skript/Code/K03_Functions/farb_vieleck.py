7b6830065a1fb3dda595a9276f55f7376692f9a8
cyrilwendl <cyrilwendl@gmail.com>
2025-02-28 22:12:24 +0100

    Setups/cyril.tex

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K03_Functions/farb_vieleck.py
@@ -0,0 +1,11 @@
+import turtle as t
+
+def farb_vieleck(ecken, farbe):
+    t.color(farbe)
+    for _ in range(ecken):
+        t.fd(50)
+        t.rt(360/ecken)
+
+farb_vieleck(3,"red")
+farb_vieleck(6,"green")
+farb_vieleck(4,"blue")
