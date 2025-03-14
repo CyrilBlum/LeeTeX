89b35f74932ed6277a54945fb893c849af3abe2f
cyrilwendl <cyrilwendl@gmail.com>
2025-03-14 12:37:45 +0100

    Updated numbers for codes with explicit line number references, updated slides

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/spirale_kreis.py
@@ -0,0 +1,22 @@
+import turtle as t
+
+# don't show turtle moving
+t.tracer(False)
+
+def vieleck(ecken, umfang):
+    for _ in range(ecken):
+        t.fd(umfang/ecken)
+        t.rt(360/ecken)
+
+def vieleck_muster(umfang):
+    while umfang>50:
+        if umfang % 20 == 0:
+            vieleck(4, umfang)
+        else:
+            vieleck(36, umfang)
+        t.rt(6)
+        umfang-=10
+
+vieleck_muster(600)
+
+t.done()
