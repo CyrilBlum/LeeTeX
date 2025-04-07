224bd1125fa374dd77c8f011ddfb8b77d6729886
cyrilwendl <cyrilwendl@gmail.com>
2025-04-07 20:52:03 +0200

    Added Python files for moodle exercises

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K03_Functions/Further Exercises/blume_petals.py
@@ -0,0 +1,20 @@
+import turtle as t
+
+def sechseck():
+    for _ in range(6):
+        t.fd(30)
+        t.rt(60)
+
+
+def blume():
+    for _ in range(10):
+        t.fd(100)
+        t.lt(60)
+        sechseck()
+        t.rt(60)
+        t.bk(100)
+        t.lt(360/10)
+
+
+blume()
+t.done()
