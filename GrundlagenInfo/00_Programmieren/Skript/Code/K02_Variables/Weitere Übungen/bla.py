b5da608868a4a0912713115e9f88e225cab99d6e
cyrilwendl <cyrilwendl@gmail.com>
2025-04-15 12:12:18 +0200

    Added further Moodle exercise solutions

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K02_Variables/Weitere Übungen/bla.py
@@ -0,0 +1,6 @@
+txt = "yes"
+for _ in range(3):
+    x = 1
+    for _ in range(5):
+        print((txt+" ")*x)
+        x += 1
--- GrundlagenInfo/00_Programmieren/Skript/Code/K03_Functions/Further Exercises/halbkreis_spirale.py
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K03_Functions/Further Exercises/halbkreis_spirale.py
@@ -3,27 +3,26 @@ import turtle as t
 def halbkreis_left(umfang):
     """macht einen Halkreis als 20-Eck, nach links drehend"""
     for i in range(10):
-        t.forward(umfang/40)
+        t.forward(umfang/20)
         t.left(360 / 20)
 
 def halbkreis_right(umfang):
     """macht einen Halkreis als 20-Eck, nach rechts drehend"""
     for i in range(10):
-        t.forward(umfang/40)
+        t.forward(umfang/20)
         t.right(360 / 20)
 
 t.speed(0)
 
 
 for _ in range(4):
-    halbkreis_left(200)
-    halbkreis_right(200)
+    halbkreis_left(100)
+    halbkreis_right(100)
 
 t.color("gold")
 for _ in range(2):
-    halbkreis_right(400)
-    halbkreis_left(400)
-        
+    halbkreis_right(200)
+    halbkreis_left(200)
     
 
 t.done()
