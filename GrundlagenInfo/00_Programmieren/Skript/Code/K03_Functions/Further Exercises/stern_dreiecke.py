0700bb4d656953fe13f0e79e437eef59aca62bdf
cyrilwendl <cyrilwendl@gmail.com>
2025-04-16 15:04:34 +0200

    Numerous updates: new exercises in conditional logic part of programming reader, new figures, slides updated, various typos in other readers fixed, etc.

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K03_Functions/Further Exercises/stern_dreiecke.py
@@ -0,0 +1,7 @@
+def zahlen(n):
+    x =1
+    for _ in range(n):
+        print(x**2)
+        x += 1   
+
+zahlen(10)
---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/fib.py
@@ -0,0 +1,7 @@
+def zahlen(n):
+    x=1
+    for _ in range(n):
+        print("Das Quadrat von "+str(x)+" ist "+str(x**2))
+        x=x+1
+
+zahlen(10)
