65dc8420631413b62b5c1771d8b6d548771644b4
cyrilwendl <cyrilwendl@gmail.com>
2025-04-19 19:34:57 +0200

    Resolved conflict

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/geschwindigkeit_angemessen.py
@@ -0,0 +1,3 @@
+def geschwindigkeit_angemessen(geschwindigkeit):
+    if 30 <= geschwindigkeit <= 100:
+        print("Die Geschwindigkeit ist angemessen")
--- GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/temperatur.py
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/temperatur.py
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream
 # Aufgabe 1: Ist die Person nicht volljährig?
+=======
+>>>>>>> Stashed changes
 def temperatur_ist_angenehm(temperatur):
     if temperatur >= 20 and temperatur <= 25:
         print("Die Temperatur ist angenehm.")
