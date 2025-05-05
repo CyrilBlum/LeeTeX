af9a21bfa4be7f0a8b235ceeac97e4e2d516f044
cyrilwendl <cyrilwendl@gmail.com>
2025-05-05 08:56:58 +0200

    Merge

--- GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/temperatur.py
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/temperatur.py
@@ -1,11 +1,11 @@
-def temperatur_ist_angenehm(temperatur):
+def temperatur_ist_unangenehm(temperatur):
     if temperatur < 10 or temperatur > 30:
         print("Unangenehme Temperatur.")
     else:
         print("Angenehme Temperatur.")
 
-temperatur_ist_angenehm(5)
-temperatur_ist_angenehm(15)
-temperatur_ist_angenehm(25)
-temperatur_ist_angenehm(35)
+temperatur_ist_unangenehm(5)
+temperatur_ist_unangenehm(15)
+temperatur_ist_unangenehm(25)
+temperatur_ist_unangenehm(35)
 
