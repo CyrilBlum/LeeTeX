eca2079acd596314de1dc10a6840c2fbc1a14882
CyrilWendl <cyril@wendl.ch>
2025-05-05 20:20:10 +0200

    Minor updates

--- GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/pos_und_gerade.py
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/pos_und_gerade.py
@@ -1,4 +1,3 @@
-# Lösung:
 def pos_und_gerade(zahl):
     if (zahl > 0) and (zahl % 2 == 0):
         print("Die Zahl ist positiv und gerade.")
--- GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/teiler_7_nicht3.py
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/teiler_7_nicht3.py
@@ -1,6 +1,3 @@
-# Alle natürlichen Zahlen zwischen 0 und 1000 ausgeben,
-# die durch 7, aber nicht durch 3 teilbar sind.
-
 for zahl in range(100):
     if zahl % 7 == 0 and zahl % 3 != 0:
         print(zahl)
