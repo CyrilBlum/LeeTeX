20b63d5f5090bd39e623bd1b2735a92f179077de
CyrilWendl <cyril@wendl.ch>
2025-05-11 21:45:59 +0200

    Minor updates to slides and reader

--- GrundlagenInfo/10_AusDatenLernen/Code/correl.py
+++ GrundlagenInfo/10_AusDatenLernen/Code/correl.py
@@ -20,7 +20,7 @@ def visualize_normalized_data(df_correl, normalize=True, save=True):
         texts.append(
             plt.text(
                 x + 0.1, y,  # Slightly shift labels to the right
-                f"x'={x:.0f}\ny'={y:.1f}" if normalize else f"x={x:.0f}\ny={y:.1f}",
+                f"x'={x:.1f}\ny'={y:.1f}" if normalize else f"x={x:.1f}\ny={y:.1f}",
                 fontsize=10,
                 color='white',
                 bbox=dict(
@@ -45,24 +45,23 @@ def visualize_normalized_data(df_correl, normalize=True, save=True):
         plt.axvline(0, color='black', linestyle='--', linewidth=1)
 
         # Add background colors for each quadrant using actual plot limits
-        plt.axhspan(0, y_max, xmin=0, xmax=(0 - x_min) / (x_max - x_min), facecolor='lightblue', alpha=0.3, label='Quadrant I')
-        plt.axhspan(0, y_max, xmin=(0 - x_min) / (x_max - x_min), xmax=x_max, facecolor='lightgreen', alpha=0.3, label='Quadrant II')
+        plt.axhspan(0, y_max, xmin=(0 - x_min) / (x_max - x_min), xmax=x_max, facecolor='lightblue', alpha=0.3, label='Quadrant I')
+        plt.axhspan(0, y_max, xmin=0, xmax=(0 - x_min) / (x_max - x_min), facecolor='lightgreen', alpha=0.3, label='Quadrant II')
         plt.axhspan(y_min, 0, xmin=0, xmax=(0 - x_min) / (x_max - x_min), facecolor='lightcoral', alpha=0.3, label='Quadrant III')
         plt.axhspan(y_min, 0, xmin=(0 - x_min) / (x_max - x_min), xmax=x_max, facecolor='moccasin', alpha=0.3, label='Quadrant IV')
 
     else:
         # Add horizontal and vertical lines at mean values
-        plt.axhline(df_correl["Straftaten"].mean(), color='ForestGreen', linestyle='--', linewidth=1, label=f'$\overline{{y}}$={df_correl["Straftaten"].mean():.2f}')
-        plt.axvline(df_correl["Polizeistreifen"].mean(), color='RoyalBlue', linestyle='--', linewidth=1, label=f'$\overline{{x}}$={df_correl["Polizeistreifen"].mean():.2f}')
+        plt.axhline(df_correl["Straftaten"].mean(), color='ForestGreen', linestyle='--', linewidth=1, label=f'$\overline{{y}}$={df_correl["Straftaten"].mean():.1f}')
+        plt.axvline(df_correl["Polizeistreifen"].mean(), color='RoyalBlue', linestyle='--', linewidth=1, label=f'$\overline{{x}}$={df_correl["Polizeistreifen"].mean():.1f}')
 
     # Set x and y limits to the actual plot limits
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
 
     # Add title, labels, grid, and legend
-    plt.title("Polizeistreifen vs. Straftaten")
-    plt.xlabel("Polizeistreifen")
-    plt.ylabel("Straftaten")
+    plt.xlabel("Anzahl Polizeistreifen")
+    plt.ylabel("Anzahl Straftaten")
     plt.grid(True)
     plt.legend()   
     if save:
