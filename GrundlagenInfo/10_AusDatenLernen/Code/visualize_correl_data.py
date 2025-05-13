b23f12dd0ed57ae58d1e4bb1c7255c35562bcfa1
CyrilWendl <cyril@wendl.ch>
2025-05-13 07:22:47 +0200

    Updated ALL slides for chapter 5, fixed numbering, further fixes in prog script and other parts

--- GrundlagenInfo/10_AusDatenLernen/Code/visualize_correl_data.py
+++ GrundlagenInfo/10_AusDatenLernen/Code/visualize_correl_data.py
@@ -12,17 +12,22 @@ def vis_correl_data(df, x, y, file_name):
     plt.tight_layout()
     plt.close()
 
+# Visualize Polizei vs. Kriminalität
 a = generiere_korrelationsdaten("polizei_vs_kriminalitaet", n=20)
 vis_correl_data(a, "Polizeistreifen", "Straftaten", "polizei_vs_kriminalitaet")
 
-# Visualize the second dataset
+# Visualize Social Media vs. Zufriedenheit
 a = generiere_korrelationsdaten("socialmedia_vs_zufriedenheit")
 vis_correl_data(a, "Beiträge pro Tag", "Zufriedenheit (0–10)", "socialmedia_vs_zufriedenheit")
 
-# Visualize the third dataset (Haarlänge vs. Notenschnitt)
+# Visualize Haarlänge vs. Notenschnitt
 df = generiere_korrelationsdaten("haarlaenge_vs_notenschnitt", n=14, seed=1)
 vis_correl_data(df, "Haarlänge (cm)", "Notenschnitt", "haarlange_vs_notenschnitt")
 
-# Visualize the third dataset (Haarlänge vs. Notenschnitt)
+# Visualize Schuhgrösse vs. Bücher
 df = generiere_korrelationsdaten("schuhgroesse_vs_buecher", n=14, seed=1)
 vis_correl_data(df, "Bücher gelesen", "Schuhgrösse", "buecher_vs_schuhgroesse")
+
+# Visualize Berufserfahrung vs. Einladungswahrscheinlichkeit
+df = generiere_korrelationsdaten("berufserfahrung_vs_einladung", n=14, seed=1)
+vis_correl_data(df, "Berufsjahre", "Einladungswahrscheinlichkeit", "berufserfahrung_vs_einladung")
