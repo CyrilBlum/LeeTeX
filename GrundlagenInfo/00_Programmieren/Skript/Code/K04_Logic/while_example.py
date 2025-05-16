a564da5c833d2e4f1a7ac34a182d9dc41abc5712
CyrilWendl <cyril@wendl.ch>
2025-05-16 12:56:55 +0200

    Updated and clarified various part, working on private files

---
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/while_example.py
@@ -0,0 +1,7 @@
+def hunger_auf_schoggi():
+    hunger = 1
+    while hunger > 0.2:
+        print("Ich habe Hunger und esse noch etwas Schokolade (aktuell Hunger: "+ str(round(hunger,2)) + ")")
+
+
+hunger_auf_schoggi()
