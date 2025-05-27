500e5ae445a390f05bc231f5ebc01c79d6faa599
ThomasGrafNumerics <thomas.graf.numerics@gmail.com>
2025-05-27 07:44:34 +0200

    corrections, removed variable names that are python keywords

--- GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/teiler_prim.py
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/teiler_prim.py
@@ -1,25 +1,26 @@
 # Eingabe der natürlichen Zahl x
-x = int(input("Geben Sie eine natürliche Zahl x ein: "))
+x = int(input("Geben Sie eine natürliche Zahl x > 1 ein:\n"))
 
 # Initialisierung der Variablen
 min_teiler = 0  # Startwert für den kleinsten Teiler
-tmax_teiler = x  # Startwert für den größten Teiler
+max_teiler = 0  # Startwert für den grössten Teiler
 i = 2  # Startwert für die Suche nach Teilern
 
-# Suche nach dem kleinsten Teiler > 1
+# kleinster Teiler > 1
 while i < x:
     if x % i == 0:
         min_teiler = i
         break
     i += 1
 
-# Suche nach dem größten Teiler < x
+# grösster Teiler < x
 if min_teiler != 0:
-    tmax_teiler = x // min_teiler
+    max_teiler = x // min_teiler
 
 # Ausgabe der Ergebnisse
 if min_teiler == 0:
-    print(f"{x} ist eine Primzahl.")
+    print(x, "ist eine Primzahl.")
 else:
-    print(f"Der kleinste Teiler von {x} größer als 1 ist: {min_teiler}")
-    print(f"Der größte Teiler von {x} kleiner als {x} ist: {tmax_teiler}")
+    print("kleinste Teiler:", min_teiler)
+    print("grösster Teiler:", max_teiler)
+
