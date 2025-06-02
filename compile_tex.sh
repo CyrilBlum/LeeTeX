ce87e5020a0141a0e0f5499a3d449bf5df204bca
CyrilWendl <cyril@wendl.ch>
2025-06-02 21:22:20 +0200

    try fix

--- compile_tex.sh
+++ compile_tex.sh
@@ -2,6 +2,8 @@
 # At the top of compile_tex.sh
 LEE_TEX_SSH_PASSWORD="$1"
 
+echo "Length: ${#LEE_TEX_SSH_PASSWORD}
+
 # This script is POSIX-compatible and works on macOS (Bash 3.x), Ubuntu, and Zsh.
 
 # Define array of document classes and their toggle positionst
