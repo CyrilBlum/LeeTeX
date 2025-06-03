993238abea72dd7c14aa455ac3b459ef09e52d99
CyrilWendl <cyril@wendl.ch>
2025-06-03 21:07:22 +0200

    Minor edits in Markov chains, cosmetic improvements in programming reader

--- GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/teiler_prim.py
+++ GrundlagenInfo/00_Programmieren/Skript/Code/K04_Logic/teiler_prim.py
@@ -1,26 +1,19 @@
-# Eingabe der natürlichen Zahl x
-x = int(input("Geben Sie eine natürliche Zahl x > 1 ein:\n"))
+x = int(input("Geben Sie eine natürliche Zahl x > 1 ein: "))
 
-# Initialisierung der Variablen
-min_teiler = 0  # Startwert für den kleinsten Teiler
-max_teiler = 0  # Startwert für den grössten Teiler
-i = 2  # Startwert für die Suche nach Teilern
+min_teiler = x
+max_teiler = 1
+teiler = 2
 
-# kleinster Teiler > 1
-while i < x:
-    if x % i == 0:
-        min_teiler = i
-        break
-    i += 1
+while teiler < x:
+    if x % teiler == 0:
+        if teiler < min_teiler:
+            min_teiler = teiler
+        if teiler > max_teiler:
+            max_teiler = teiler
+    teiler += 1
 
-# grösster Teiler < x
-if min_teiler != 0:
-    max_teiler = x // min_teiler
-
-# Ausgabe der Ergebnisse
-if min_teiler == 0:
-    print(x, "ist eine Primzahl.")
+if min_teiler == x and max_teiler == 1:
+    print(f"{x} ist eine Primzahl.")
 else:
-    print("kleinste Teiler:", min_teiler)
-    print("grösster Teiler:", max_teiler)
-
+    print(f"Der kleinste echte Teiler von {x} ist {min_teiler}.")
+    print(f"Der größte echte Teiler von {x} ist {max_teiler}.")
