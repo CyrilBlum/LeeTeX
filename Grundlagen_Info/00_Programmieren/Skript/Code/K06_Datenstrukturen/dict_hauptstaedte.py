6f5c850a71071c960b1773ca763d10de1046a46e
CyrilWendl <cyril@wendl.ch>
2025-06-18 14:48:18 +0200

    Minor edits, moving Python answers to external files

---
+++ Grundlagen_Info/00_Programmieren/Skript/Code/K06_Datenstrukturen/dict_hauptstaedte.py
@@ -0,0 +1,10 @@
+hauptstaedte = {
+    "Schweiz": "Hier gibt es keine Hauptstadt, nur eine Bundesstadt.",
+    "Deutschland": "Berlin",
+    "Frankreich": "Paris"
+}
+land = input("Land eingeben: ")
+if land in hauptstaedte:
+    print("Die Hauptstadt ist:", hauptstaedte[land])
+else:
+    print("Land nicht gefunden.")
