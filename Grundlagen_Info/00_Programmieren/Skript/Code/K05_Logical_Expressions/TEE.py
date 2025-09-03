9b996db47ed72c0b8385920c925b0ce31d1675c8
CyrilWendl <cyril@wendl.ch>
2025-09-03 15:00:06 +0200

    cleaned up python files, added chapter 8 "Anwendungen" (BMR / TDEE exercises with return) , added new exercises for return with lists and dicts at the end of chapter 6

--- Grundlagen_Info/00_Programmieren/Skript/Code/K05_Logical_Expressions/TEE.py
+++
@@ -1,7 +0,0 @@
-bmr = durchschnitt_bmr(gewicht, groesse, alter, geschlecht, koerperfettanteil)
-l = berechne_leistungsumsatz(l_zeit, l_kcal)
-
-def berechne_total(bmr, l):
-    return bmr + l
-
-total = berechne_total(bmr, l)
