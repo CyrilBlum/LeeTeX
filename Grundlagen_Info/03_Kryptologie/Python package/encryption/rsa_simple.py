9316614b249b7a443ed89aee3a8ac73f87b39405
CyrilWendl <cyril@wendl.ch>
2025-11-10 18:04:28 +0100

    Kryptologie-Skript - Lernziele vervollständigt - Klärungen zu RSA - Challenge-Aufgaben RSA

---
+++ Grundlagen_Info/03_Kryptologie/Python package/encryption/rsa_simple.py
@@ -0,0 +1,24 @@
+# Primzahlen
+p = 109
+q = 113
+# Berechnung n und phi(n)
+n = p * q
+phi_n = (p - 1) * (q - 1)
+print("n:", n)
+print("phi(n):", phi_n)
+# Wahl von e (muss teilerfremd zu phi(n) sein)
+e = 5  # 5 ist teilerfremd zu phi(n)
+# Berechnung von d
+d = pow(e, -1, phi_n)
+# Öffentlicher und privater Schlüssel
+public_key = (e, n)
+private_key = (d, n)
+print("Öffentlicher Schlüssel:", public_key)
+print("Privater Schlüssel:", private_key)
+# Nachricht verschlüsseln
+m = 1234  # Beispielnachricht
+c = pow(m, e, n)
+print("Verschlüsselte Nachricht:", c)
+# Nachricht entschlüsseln
+decrypted_m = pow(c, d, n)
+print("Entschlüsselte Nachricht:", decrypted_m)
