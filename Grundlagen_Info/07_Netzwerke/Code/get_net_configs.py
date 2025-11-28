afc3e1fc3635ca8cf386355935817bf30514565f
CyrilWendl <cyril@wendl.ch>
2025-11-28 17:36:52 +0100

    Various network updates

--- Grundlagen_Info/07_Netzwerke/Code/get_net_configs.py
+++ Grundlagen_Info/07_Netzwerke/Code/get_net_configs.py
@@ -1,20 +1,20 @@
-# this file will get the MAC address of the machine, the local IP and the global IP address.
+# Gibt die MAC-Adresse, lokale und globale IP-Adresse des Geräts aus.
 import uuid
 import socket
 import requests
 
-# Get MAC address
+# Gibt die MAC-Adresse dieses Geräts aus
 mac = uuid.getnode()
 mac_hex = ":".join(f"{(mac >> ele) & 0xff:02x}" for ele in range(40, -1, -8))
 print(f"{'MAC-Adresse dieses Geräts:':35} {mac_hex}")
 
-# Get local IP address
+# Gibt die lokale IP-Adresse dieses Geräts aus
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))  # Google DNS, no packets sent
+s.connect(("8.8.8.8", 80))  # Google DNS-Server, kein Datenaustausch
 ip_local = s.getsockname()[0]
 s.close()
 print(f"{'Lokale IP-Adresse dieses Geräts:':35} {ip_local}")
 
-# Get global IP address
+# Gibt die globale IP-Adresse dieses Geräts aus
 ip_global = requests.get("https://api.ipify.org").text
 print(f"{'Globale IP-Adresse dieses Geräts:':35} {ip_global}")
