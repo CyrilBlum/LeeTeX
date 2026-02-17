e8add70eae8d53dfd9bcd62adee44cdec6e95565
CyrilWendl <cyril@wendl.ch>
2026-02-17 17:48:56 +0100

    pip installation instructions homogeneous, minor adjustments

--- Grundlagen_Info/00_Programmieren/Skript/Code/K03_Variables_Debugging/s_to_y.py
+++ Grundlagen_Info/00_Programmieren/Skript/Code/K03_Variables_Debugging/s_to_y.py
@@ -2,9 +2,9 @@ sekunden_gesamt = 123456789
 
 # Umrechnungsfaktoren
 SEK_PRO_MINUTE = 60
-SEK_PRO_STUNDE = 3600
-SEK_PRO_TAG = 86400
-SEK_PRO_JAHR = 31536000
+SEK_PRO_STUNDE = 60 * SEK_PRO_MINUTE
+SEK_PRO_TAG = 24 * SEK_PRO_STUNDE
+SEK_PRO_JAHR = 365 * SEK_PRO_TAG
 
 # Jahre berechnen und den Rest behalten
 jahre = sekunden_gesamt // SEK_PRO_JAHR
