ff8b7c6aa9f97414f68388f9c1286dfe2993a216
CyrilWendl <cyril@wendl.ch>
2026-02-18 10:25:12 +0100

    various fixes and clarifications

---
+++ Grundlagen_Info/07_Netzwerke/Code/server.py
@@ -0,0 +1,27 @@
+import socket
+
+PORT = 12345
+
+# Server-Socket erstellen
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Erstelle einen TCP/IP Socket
+server.setsockopt(
+    socket.SOL_SOCKET, socket.SO_REUSEADDR, 1
+)  # Erlaube die Wiederverwendung des Ports (nützlich beim Neustart des Servers)
+server.bind(("localhost", PORT))  # Binde den Socket an localhost und den definierten Port
+server.listen(1)  # Warte auf eingehende Verbindungen (maximal 1 in der Warteschlange)
+print("Warte auf Verbindung...")  # Server wartet auf Verbindungen
+
+# Verbindung akzeptieren
+conn, addr = server.accept()  # Akzeptiere eine eingehende Verbindung
+print(f"Verbunden mit {addr[0]} über Port {addr[1]}")  # Zeige die Adresse und den Port des verbundenen Clients an
+
+# Nachricht vom Client empfangen
+nachricht = conn.recv(1024).decode()  # Empfange bis zu 1024 Bytes
+print(f"Empfangen: {nachricht}")  # Zeige die empfangene Nachricht an
+
+# Antwort an den Client senden
+nachricht = input("Nachricht an den Client eingeben: ")  # Lese eine Nachricht von der Konsole ein
+conn.send(nachricht.encode())  # Sende die eingegebene Nachricht an den Client
+
+conn.close()
+server.close()
