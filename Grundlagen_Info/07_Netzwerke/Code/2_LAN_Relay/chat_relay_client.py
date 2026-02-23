4669894cd7aa7a9e9f1bdf25ba83a88c240466c8
Cyril Wendl <cyril@wendl.ch>
2026-02-23 20:44:09 +0100

    simplifications and optimizations

--- Grundlagen_Info/07_Netzwerke/Code/2_LAN_Relay/chat_relay_client.py
+++ Grundlagen_Info/07_Netzwerke/Code/2_LAN_Relay/chat_relay_client.py
@@ -1,19 +1,19 @@
 import socket
 import threading # wird benötigt, um Nachrichten gleichzeitig senden sowie empfangen zu können.
+# library importieren, um verzögerung (abwarten) zu ermöglichen, damit der Server Zeit hat, die Nachricht zu verarbeiten und zu senden, bevor der Client versucht, sie zu empfangen.
+import time
 
-SERVER_IP = "192.168.1.23"  # IP-Adresse des Relay-Servers im LAN
+SERVER_IP = "192.168.1.18"  # IP-Adresse des Relay-Servers im LAN
 SERVER_PORT = 12345
 
 # Client-Socket erstellen und mit dem Server verbinden
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Erstelle einen TCP-IP Socket
 client.connect((SERVER_IP, SERVER_PORT))  # Verbinde den Socket mit dem Server
 
-EMPFAENGER_IP = input("Empfänger-IP eingeben: ")  # Lese die IP-Adresse des Empfängers von der Konsole ein
-
-def receive_messages(sock):
+def receive_messages():
     while True:
         try:
-            nachricht = sock.recv(1024).decode()
+            nachricht = client.recv(1024).decode()
             if nachricht:
                 # Prüfe ob die Nachricht vom Format "From IP: msg" ist (echte Chat-Nachricht)
                 if nachricht.startswith("From "):
@@ -31,16 +31,19 @@ def receive_messages(sock):
             print(f"\nFehler beim Empfangen der Nachricht: {e}")
             break
 # Starte einen separaten Thread, um Nachrichten vom Server zu empfangen, damit der Hauptthread weiterhin Eingaben vom Benutzer akzeptieren kann. Deamon Thread, damit er automatisch beendet wird, wenn der Hauptthread endet.
-threading.Thread(target=receive_messages, args=(client), daemon=True).start()
+threading.Thread(target=receive_messages, daemon=True).start()
+time.sleep(1)  # Kurze Verzögerung, damit der Empfangsthread Zeit hat, die Einführungsnachrichten zu empfangen und anzuzeigen, bevor der Benutzer zur Eingabe aufgefordert wird.
 
 # Starte eine Endlosschleife, um Nachrichten an den Server zu senden. Der Benutzer kann jederzeit mit Strg+C die Verbindung beenden.
 try:
     # Endlosschleife für den Nachrichtenaustausch
     while True:
+        empfaenger_ip = input("Empfänger-IP eingeben: ")
+
         nachricht = input("Nachricht: ")
-        nachricht = f"{EMPFAENGER_IP}:{nachricht}"
-        client.send(nachricht.encode())
+        client.send(f"{empfaenger_ip}:{nachricht}".encode())
         # Antworten vom Server werden vom receive_messages Thread empfangen
+        time.sleep(1)  # Kurze Verzögerung, damit der Empfangsthread Zeit hat, die Antwort zu empfangen und anzuzeigen, bevor der Benutzer zur nächsten Eingabe aufgefordert wird.
 
 except KeyboardInterrupt:
     print(f"Verbindung zum Server ({SERVER_IP}) wird getrennt.")
