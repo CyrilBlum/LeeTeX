29dce3bea9e719ceedc0e881e62cd41b8e5589bf
ThomasGrafNumerics <thomas.graf.numerics@gmail.com>
2026-03-08 16:29:29 +0100

    improvements

---
+++ EF/Complexity/Code/anstossen.py
@@ -0,0 +1,7 @@
+def jeder_mit_jedem(personen):
+    # jede Person soll mit jeder anderen Person genau einmal anstossen
+    # keine Person soll mit sich selbst anstossen
+    n = len(personen)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            print(personen[i], "und", personen[j], "stossen an.")
---
+++ EF/Complexity/Code/e.py
@@ -0,0 +1,6 @@
+def e(n):
+    k = 0
+    summe = 0
+    while k <= n:
+        summe += k
+        k += 1
