1ca47413be6a0cef1c277348b54039e142f3ec25
ThomasGrafNumerics <thomas.graf.numerics@gmail.com>
2026-03-26 15:09:17 +0100

    solutions to exercises in slides

--- EF/Complexity/Code/bubble_sort.py
+++ EF/Complexity/Code/bubble_sort.py
@@ -1,6 +1,6 @@
 def bubble_sort(A):
     n = len(A)
-    for i in range(n):
+    for i in range(n - 1):
         for j in range(0, n - i - 1):
             if A[j] > A[j + 1]:
                 A[j], A[j + 1] = A[j + 1], A[j]
